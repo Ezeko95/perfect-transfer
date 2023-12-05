eb227357dec1a3104c3e24fff4bfe0a60932144a
Ezeko95 <ciangianluca@hotmail.com>
2023-12-05 18:21:03 +0100

    fixed navbar with company logo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import NavBar from "./components/NavBar";
+import Header from "./components/Header";
 
 export default function Home() {
   return (
     <div>
       <NavBar></NavBar>
+      <Header></Header>
     </div>
   );
 }
