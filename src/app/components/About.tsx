00ee68c99a26ec773d2d28733a7b4b0ca908fe26
Ezeko95 <ciangianluca@hotmail.com>
2023-12-16 10:58:03 +0100

    tests

--- src/app/components/About.tsx
+++ src/app/components/About.tsx
@@ -8,7 +8,7 @@ const About: React.FC<AboutUsProps> = ({ sectionRef1 }) => {
   const [scrollY, setScrollY] = useState(0);
 
   const logit = () => {
-    setScrollY(window.pageYOffset);
+    setScrollY(window.scrollY);
   };
 
   useEffect(() => {
@@ -23,7 +23,7 @@ const About: React.FC<AboutUsProps> = ({ sectionRef1 }) => {
   return (
     <div className="font-garamond" id="about" ref={sectionRef1}>
       <div className="flex flex-row items-center justify-center px-10 py-8 md:px-20 md:py-16 lg:px-36 lg:py-12 gap-x-6 md:gap-x-12 lg:gap-x-20">
-        <div className={` ${scrollY > 350 ? "slide-in-right" : ""}`}>
+        <div className={` ${scrollY > 20 ? "slide-in-right" : ""}`}>
           <h2 className="text-4xl font-garamond font-light italic text-white py-4 px-6 rounded-t-sm max-w-sm bg-green-700">
             Misión
           </h2>
@@ -36,12 +36,12 @@ const About: React.FC<AboutUsProps> = ({ sectionRef1 }) => {
             excepcional que impulse su éxito.
           </p>
         </div>
-        <div className={` ${scrollY > 350 ? "slide-in-right" : ""}`}>
+        <div className={` ${scrollY > 20 ? "slide-in-right" : ""}`}>
           <img src="/prueba.jpg" width={"620"} alt="prueba" />
         </div>
       </div>
       <div className="flex flex-row-reverse items-center justify-center px-10 py-8 md:px-20 md:py-16 lg:px-36 lg:py-12 gap-x-6 md:gap-x-12 lg:gap-x-20">
-        <div className={` ${scrollY > 745 ? "slide-in-left" : ""}`}>
+        <div className={` ${scrollY > 400 ? "slide-in-left" : ""}`}>
           <h2 className="text-4xl max-w-sm font-garamond font-light italic text-white py-4 px-6 rounded-t-sm bg-green-700">
             Nuestra visión
           </h2>
@@ -53,11 +53,10 @@ const About: React.FC<AboutUsProps> = ({ sectionRef1 }) => {
             catalizador de la innovación en la industria de impresión gráfica.
           </p>
         </div>
-        <div className={` ${scrollY > 745 ? "slide-in-left" : ""}`}>
+        <div className={` ${scrollY > 400 ? "slide-in-left" : ""}`}>
           <img src="/prueba2.jpg" alt="prueba2" width={"620"} />
         </div>
       </div>
-      
     </div>
   );
 };
