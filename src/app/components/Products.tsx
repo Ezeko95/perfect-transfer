01cd9a3d85bf2b638107f3cf9ed002d7cf2275c2
Ezeko95 <ciangianluca@hotmail.com>
2024-01-02 20:19:52 +0100

    responsiveness

--- src/app/components/Products.tsx
+++ src/app/components/Products.tsx
@@ -42,7 +42,7 @@ const Products: React.FC<ProductsProps> = ({ sectionRef2 }) => {
         blockScroll ? "overflow-hidden" : ""
       }`}
     >
-      <div className="grid grid-cols-4 max-w-8xl justify-center py-10 gap-10">
+      <div className="grid grid-cols-2 md:grid-cols-4 max-w-8xl justify-center py-10 gap-10">
         {productCategories.map((category: ProductCategory) => (
           <ProductCard
             key={category.id}
@@ -54,7 +54,7 @@ const Products: React.FC<ProductsProps> = ({ sectionRef2 }) => {
       <div className="flex items-center justify-center">
         <a
           onClick={() => window.open("/catalogo.pdf", "_blank")}
-          className="inline-flex items-center gap-x-2 mt-10 bg-green-700 text-white font-garamond font-extrabold text-2xl transition-transform duration-300 transform hover:scale-110 py-4 px-6 rounded-md mb-20"
+          className="inline-flex items-center gap-x-2 mt-10 bg-green-700 text-white font-garamond font-extrabold text-lg md:text-2xl transition-transform duration-300 transform hover:scale-110 py-4 px-6 rounded-md mb-20"
         >
           <FaFilePdf />
           Ver catálogo
