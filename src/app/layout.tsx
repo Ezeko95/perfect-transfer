55084355574c134d56e84323591d7595553a96c7
Ezeko95 <ciangianluca@hotmail.com>
2024-01-11 19:21:37 +0100

    meta image res

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,8 +19,8 @@ export default function RootLayout({
     <html lang="en" className="scroll-smooth">
       <head>
         <meta property="og:image" content="/logo-invertido.png" />
-        <meta property="og:image:width" content="600" />
-        <meta property="og:image:height" content="400" />
+        <meta property="og:image:width" content="1200" />
+        <meta property="og:image:height" content="630" />
         <meta property="og:image:alt" content="Company Logo" />
         <link rel="icon" href="/logo-miniatura.png" sizes="logo" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
