0dd8a7cb43bd789236cd88b2f53f0ae5cd1358e3
Gianluca Cianchi <97952175+Ezeko95@users.noreply.github.com>
2024-06-05 16:11:33 +0000

    Analaytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import { Analytics } from "@vercel/analytics/react";
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -39,7 +40,9 @@ export default function RootLayout({
           rel="stylesheet"
         />
       </head>
-      <body>{children}</body>
+      <body>{children}
+      <Analytics/>
+      </body>
     </html>
   );
 }
